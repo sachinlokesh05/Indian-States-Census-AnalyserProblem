1d5ba415e8deaf9c55ee4c98b01eeb7520e3bc2a
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-04-27 18:14:07 +0530

    [sachin]Add.added csv file opening functionality

--- censusAnalyser.py
+++ censusAnalyser.py
@@ -1,5 +1,17 @@
+import pandas as pd
+import os
 class StateCensusAnalyser:
-    pass
+    
+    def __init__(self, filename):
+        target_path_1 = os.path.join(os.path.dirname(__file__), filename)
+        datainput = pd.read_csv(target_path_1)
+        #https://www.kaggle.com/danofer/india-census#india-districts-census-2011.csv
+        print(datainput)
+
 
 class CSVStateCensus:
     pass    
+
+if __name__ == "__main__":
+    Sca = StateCensusAnalyser(filename='indian_census_data_2011.csv')
+    print(Sca)
