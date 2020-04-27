3db008e235b61df6d1ae560028d0fcfb1e2105c6
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-04-27 18:30:15 +0530

    [sachin]Add.added loading csv file data functionality

--- censusAnalyser.py
+++ censusAnalyser.py
@@ -4,14 +4,14 @@ class StateCensusAnalyser:
     
     def __init__(self, filename):
         target_path_1 = os.path.join(os.path.dirname(__file__), filename)
-        datainput = pd.read_csv(target_path_1)
-        #https://www.kaggle.com/danofer/india-census#india-districts-census-2011.csv
-        print(datainput)
+        self.csv_data = pd.read_csv(target_path_1)
 
 
-class CSVStateCensus:
-    pass    
+class CSVStateCensus(StateCensusAnalyser):
+    
+    def __init__(self, filename):
+        super().__init__(filename)   
 
 if __name__ == "__main__":
-    Sca = StateCensusAnalyser(filename='indian_census_data_2011.csv')
-    print(Sca)
+    Sca = CSVStateCensus(filename='indian_census_data_2011.csv')
+    print(Sca.csv_data)
