ac6e06c03521c4a3097a1ffdb44e9b50b92069e4
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-04-27 22:05:50 +0530

    [sachin]Add.added counting number of records functionality

--- censusAnalyser.py
+++ censusAnalyser.py
@@ -1,25 +1,27 @@
 import pandas as pd
+import pathlib
+from path import Path
 import csv
 import os
 class StateCensusAnalyser:
     
     def __init__(self, filename):
-        target_path_1 = os.path.join(os.path.dirname(__file__), filename)
-        self.csv_data = csv.DictReader(open(target_path_1,mode='r'))
-        self.csv_data1=pd.read_csv(target_path_1)
+        target_path_1 =  os.path.join(os.path.dirname(__file__), filename)
+        print(target_path_1)
+        return pd.read_csv(target_path_1)
 
 
 class CSVStateCensus(StateCensusAnalyser):
     
     def __init__(self, filename):
-        self.a = super().__init__(filename)
-    def do_something(self):
-        df_dict=self.csv_data1.to_dict()
-        print(df_dict['State name'])
-        #count number of rows
-        print(df_dict.__len__())
+        self.csv_data = super().__init__(filename)
+        
+    def getNumberofrecordes(self):
+        return len(self.csv_data)
+        
+    
 
 if __name__ == "__main__" :
 
-    Sca = CSVStateCensus(filename='indian_census_data_2011.csv')
-    Sca.do_something()
+    Sca = CSVStateCensus(filename='StateCensusData.csv')
+    print(Sca.getNumberofrecordes())
